64b8578dc1ab99e8fe0fb665812d6e0d60226084
temirgaleevroman <roman.temirgaleev@gmail.com>
2024-10-25 17:54:44 +0300

    tests opencart

--- tests/test_opencart.py
+++ tests/test_opencart.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from locators import
+from locators import *
 
 
 # Таймаут для ожиданий
